fc021e9dc4a5efa4d92df963a6235390486058ac
tynopet <dimangud1994@gmail.com>
2024-01-23 23:49:10 +0000

    Use real location

--- src/index.js
+++ src/index.js
@@ -13,11 +13,27 @@ function main() {
 
   const geom = new THREE.BoxGeometry(20, 20, 20);
   const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const box = new THREE.Mesh(geom, mtl);
 
-  arjs.add(box, -0.72, 51.051);
+  const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
 
-  arjs.fakeGps(-0.72, 51.05);
+  let fetched = false;
+
+  arjs.on("gpsupdate", async(pos) => {
+    if(!fetched) {
+      const response = await fetch(`https://hikar.org/webapp/map?bbox=${pos.coords.longitude-0.01},${pos.coords.latitude-0.01},${pos.coords.longitude+0.01},${pos.coords.latitude+0.01}&layers=poi&outProj=4326`);
+
+      const geojson = await response.json();
+
+      geojson.features.forEach(feature => {
+        const box = new THREE.Mesh(geom, mtl);
+        arjs.add(box, feature.geometry.coordinates[0], feature.geometry.coordinates[1]);            
+      });
+
+      fetched = true;
+    }
+});
+
+  arjs.startGps();
 
   function render() {
     if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
@@ -28,6 +44,8 @@ function main() {
       camera.aspect = aspect;
       camera.updateProjectionMatrix();
     }
+
+    deviceOrientationControls.update();
 
     cam.update();
     renderer.render(scene, camera);
